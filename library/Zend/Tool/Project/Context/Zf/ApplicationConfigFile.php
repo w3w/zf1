2d04fc77f0b74e4112b1c0e75203ef98661efd84
hungtd <trinhduchung@gmail.com>
2022-11-14 15:58:24 +0700

    Add missing properties to classes
    Fixing issues on PHP 8.2 like
    > Deprecated: Creation of dynamic property {x} is deprecated
    
    Signed-off-by: Elan Ruusamäe <glen@pld-linux.org>
    Co-authored-by: Elan Ruusamäe <glen@pld-linux.org>

--- library/Zend/Tool/Project/Context/Zf/ApplicationConfigFile.php
+++ library/Zend/Tool/Project/Context/Zf/ApplicationConfigFile.php
@@ -38,6 +38,10 @@
  */
 class Zend_Tool_Project_Context_Zf_ApplicationConfigFile extends Zend_Tool_Project_Context_Filesystem_File
 {
+    /**
+     * @var string | null
+     */
+    protected $_type;
 
     /**
      * @var string
